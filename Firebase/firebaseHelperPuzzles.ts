dbde9267bc94ac81f19dd69bdbf1a44da0628095
Ronald <mundell.r@northeastern.edu>
2025-03-17 15:09:32 -0700

    remove unessecary imports

--- Firebase/firebaseHelperPuzzles.ts
+++ Firebase/firebaseHelperPuzzles.ts
@@ -1,4 +1,4 @@
-import { collection, addDoc, doc, deleteDoc, getDocs, getDoc, updateDoc, setDoc } from "firebase/firestore"; 
+import { collection, addDoc, doc, deleteDoc, getDocs } from "firebase/firestore"; 
 import { database } from "./firebaseSetup";
 import { CollectionPuzzle, PuzzleData, geoLocationData } from "@/Firebase/DataStructures";
 
