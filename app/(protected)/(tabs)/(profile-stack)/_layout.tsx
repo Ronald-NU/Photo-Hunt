8730bde0c7c62ea2a8463d6684d6bb4ccdede7a2
Sisi You <you.si@northeastern.edu>
2025-03-18 16:40:19 -0700

    Add ProfileStackLayout component

---
+++ app/(protected)/(tabs)/(profile-stack)/_layout.tsx
@@ -0,0 +1,12 @@
+import { Stack } from "expo-router";
+
+export default function ProfileStackLayout() {
+  return (
+    <Stack>
+      <Stack.Screen name="profile" options={{ title: "Profile", headerShown: false }} />
+      <Stack.Screen name="myPuzzles" options={{ title: "My Puzzles" }} />
+      <Stack.Screen name="viewFriends" options={{ title: "View Friends" }} />
+      <Stack.Screen name="reminder" options={{ title: "Reminders" }} />
+    </Stack>
+  );
+}
