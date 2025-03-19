bf7f3cc86bbbcc826f6d7dc9888ba589fc24c1b0
Sisi You <you.si@northeastern.edu>
2025-03-18 19:06:05 -0700

    Add BottomTabNavigator component

---
+++ components/BottomTabNavigator.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Tabs } from "expo-router";
+import { Ionicons } from "@expo/vector-icons";
+
+export default function BottomTabNavigator() {
+  return (
+    <Tabs screenOptions={{ headerShown: false }}>
+      <Tabs.Screen
+        name="(newGame-stack)"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="add-circle-outline" size={size} color={color} />
+          ),
+          title: "New Game",
+        }}
+      />
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="map-outline" size={size} color={color} />
+          ),
+          title: "Map",
+        }}
+      />
+      <Tabs.Screen
+        name="(profile-stack)"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="person-outline" size={size} color={color} />
+          ),
+          title: "Profile",
+        }}
+      />
+    </Tabs>
+  );
+}
