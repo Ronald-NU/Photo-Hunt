af536a7fb2e9f6f64bf79ab1371ef7485ba2acc8
Sisi You <you.si@northeastern.edu>
2025-03-18 17:39:50 -0700

    Add expo-image-picker dependency

--- app/(protected)/(tabs)/_layout.tsx
+++ app/(protected)/(tabs)/_layout.tsx
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import { Ionicons } from '@expo/vector-icons';
 
 import { Tabs } from 'expo-router';
@@ -8,7 +8,7 @@ export default function Layout() {
     <Tabs screenOptions={{ headerShown: false }} initialRouteName="(mapstack)">
       <Tabs.Screen
         name="(newgamestack)"
-=======
+
 // app/(protected)/(tabs)/_layout.tsx
 import { Tabs } from 'expo-router';
 import { Ionicons } from '@expo/vector-icons';
@@ -18,7 +18,7 @@ export default function TabsLayout() {
     <Tabs screenOptions={{ headerShown: false }}>
       <Tabs.Screen
         name="(newGame-stack)"
->>>>>>> 383b733 (Refactor tab navigation and screens)
+
         options={{
           tabBarIcon: ({ color, size }) => (
             <Ionicons name="add-circle-outline" size={size} color={color} />
@@ -52,8 +52,7 @@ export default function TabsLayout() {
           title: "Profile",
         }}
       />
-<<<<<<< HEAD
-=======
+
       
       {/* Use one approach or the other, not both */}
       <Tabs.Screen
@@ -62,7 +61,7 @@ export default function TabsLayout() {
           tabBarButton: () => null,
         }}
       />
->>>>>>> 383b733 (Refactor tab navigation and screens)
+
     </Tabs>
   );
 }
