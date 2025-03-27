18a912e7fe1777f950d3e4488a6176bd3d100b2a
Ronald <mundell.r@northeastern.edu>
2025-03-27 13:14:16 -0700

    Added Loading screen on intial load of app to check if someone previously was logged in

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,14 @@
+import { colors } from "@/constants/Colors";
+import { GeneralStyle } from "@/constants/Styles";
 import { auth } from "@/Firebase/firebaseSetup";
 import { router, Stack, useSegments } from "expo-router";
 import { onAuthStateChanged } from "firebase/auth";
 import { useEffect, useState } from "react";
+import { ActivityIndicator, View } from "react-native";
 
 export default function Layout() {
     const [userLoggedIn, setUserLoggedIn] = useState(false);
+    const [loading, setLoading] = useState(true);
     const segments = useSegments();
 
     useEffect(() => {
@@ -12,9 +16,11 @@ export default function Layout() {
           if (user) {
           // User is signed in, see docs for a list of available properties
           // https://firebase.google.com/docs/reference/js/firebase.User
+            setLoading(false);
             setUserLoggedIn(true);
           } else {
           // User is signed out
+            setLoading(false);
             setUserLoggedIn(false);
           }
         })
@@ -30,6 +36,14 @@ export default function Layout() {
         router.replace("/(auth)/login");
         }
    }, [userLoggedIn]);
+
+   if (loading) {
+    return (
+      <View style={GeneralStyle.container}>
+        <ActivityIndicator size="large" color={colors.Primary} />
+      </View>
+    );
+  }
    
     return   (
     <Stack screenOptions={{ headerShown: false, animation: "fade" }}>
