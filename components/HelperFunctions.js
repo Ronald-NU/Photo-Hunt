e4b2577ba33255be5da82c22c89a8e2c83db32bd
Ronald <mundell.r@northeastern.edu>
2025-04-13 10:36:36 -0700

    Score make sure works correctly.

--- components/HelperFunctions.js
+++ components/HelperFunctions.js
@@ -21,6 +21,7 @@ const haversineDistance = (coord1, coord2) => {
 
   export const scoreCalulation = (moves, size) => {
     var score = size*500*size;
-    score = score - Math.floor(moves/100)*100-Math.floor(moves/10)*25;
+    score = score - Math.floor(moves/100)*100;
+    score = score - Math.floor(moves/10)*25;
   return score
   }
